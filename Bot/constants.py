085a047afa0efc5d298ed5ae542d0b32424be5fe
Grymlock <39034241+Grymlock@users.noreply.github.com>
2018-05-13 14:54:10 -0400

    Add files via upload

---
+++ Bot/constants.py
@@ -0,0 +1,15 @@
+
+
+version="0.19"
+
+ownerID=317355965542432769
+
+admins=[317355965542432769]
+
+helpers=[]
+
+cogs=["reactions","admin","fun","target","servers","cogs","utilities"]
+
+goodServers=[390557821487415307,374242074037452800]
+
+token='NDI2NTYwNDk3NzgxODMzNzQ4.DcALhQ.brkoQtrgduoa-SZrJLJ38cFh-fg'
